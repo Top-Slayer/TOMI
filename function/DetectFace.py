97bde6e26bded5c3ce2c7309bce0a048d66bd7f6
litar2 <dalavonephimsipasom@gmail.com>
2023-12-21 16:38:07 +0700

    sds

--- function/DetectFace.py
+++ function/DetectFace.py
@@ -3,7 +3,7 @@ from threading import Thread
 
 from function import PlaySound
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
