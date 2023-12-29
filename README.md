355635d97dbe96ab91ed09c6749af9318b3ecf8a
Top-Slayer <dragontops55@gmail.com>
2023-12-29 23:07:37 +0700

    [Modifier] Debug some bug

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Maybe it's my graduate project hahahaha~~~
 command:
 Run virtual environment: 
-- .\TOMI-env\Scripts\activate  # Warning: You have to located on default path when accessed into this project.
+- .\TOMI-env\Scripts\activate  # For window os
+- source TOMI-env/Scripts/activate # For git or linux kernel
+Warning: You have to located on default path when accessed into this project.
 Stop virtual environment: 
 - deactivate
--- run.sh
+++ run.sh
@@ -1,20 +1,29 @@
 setup_install(){
     if pip list | grep -Fq "virtualenv"; then
         echo " Library [ virtualenv ] is installed"
-        pip show virtualenv
+        pip list | grep virtualenv
     else
         echo " Installing..." 
         pip install virtualenv
     fi
 
-    cd TOMI-env
-    source Scripts/activate
-    cd ..
-    echo -e " TOMI processing...\n"
+    if [ -d "TOMI-env" ]; then
+        echo " Folder TOMI-env exists."
+    else
+        virtualenv TOMI-env
+    fi
+
+    source TOMI-env/Scripts/activate
 }
 
 setup_install
 
-python TOMI-project/Main.py
-# echo -e "\nEnd processing..."
-# read
+pip install -r requirements.txt
+pip freeze > requirements.txt
+echo -e "\n ------------------------------------------------ \n"
+echo -e " TOMI processing...\n"
+
+cd TOMI-project
+python Main.py
+echo -e "\nEnd processing..."
+read
