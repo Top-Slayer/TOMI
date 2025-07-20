918023e0e9c82653ec1d980a6a1d1c570292845d
41. Lattikone SITTHIPHONE 3COM2 <dragontops55@gmail.com>
2025-07-20 19:13:34 +0000

    modify: change vc module and add more ignore

--- core-process/functions/voice_changer.py
+++ core-process/functions/voice_changer.py
@@ -5,39 +5,42 @@ import torch
 import numpy as np
 from scipy.io.wavfile import write as wav_write
 from rvc_python.infer import RVCInference
-from scipy.io import wavfile
+from io import BytesIO
 
 
-text = "ມື້ນີ້ທ່ານຮູ້ສຶກເມື້ອຍໆນະໃຫ້ໂທມິຊ່ວຍຫຍັງໄດ້ແດ່"
+def change_voice():
+    text = "ມື້ນີ້ທ່ານຮູ້ສຶກເມື້ອຍໆນະໃຫ້ໂທມິຊ່ວຍຫຍັງໄດ້ແດ່"
 
-model = VitsModel.from_pretrained("facebook/mms-tts-lao")
-tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-lao")
+    model = VitsModel.from_pretrained("facebook/mms-tts-lao")
+    tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-lao")
 
-inputs = tokenizer(text, return_tensors="pt")
-inputs["input_ids"] = inputs["input_ids"].long()
+    inputs = tokenizer(text, return_tensors="pt")
+    inputs["input_ids"] = inputs["input_ids"].long()
 
-if inputs["input_ids"].shape[1] == 0:
-    print("[TTS] ❌ Tokenizer returned empty input.")
-    exit(1)
+    if inputs["input_ids"].shape[1] == 0:
+        print("[TTS] ❌ Tokenizer returned empty input.")
+        exit(1)
 
-with torch.no_grad():
-    int16_datas = model(**inputs).waveform
+    with torch.no_grad():
+        int16_datas = model(**inputs).waveform
 
-int16_datas = (int16_datas.cpu().numpy() * 32767).astype(np.int16).squeeze()
+    int16_datas = (int16_datas.cpu().numpy() * 32767).astype(np.int16).squeeze()
 
-print(
-    f"TTS output info: {int16_datas} size: {len(int16_datas)} shape: {int16_datas.shape}"
-)
+    print(
+        f"TTS output info: {int16_datas} size: {len(int16_datas)} shape: {int16_datas.shape}"
+    )
 
-# test write wav file
-wav_write("audio.wav", rate=model.config.sampling_rate, data=int16_datas)
+    # test write wav file
+    buffer = BytesIO()
+    wav_write(buffer, rate=model.config.sampling_rate, data=int16_datas)
+    buffer.seek(0)
 
 
-rvc = RVCInference(
-    device="cuda:0",
-    model_path="mikuAI/MikuAI.pth",
-    index_path="mikuAI/added_IVF3010_Flat_nprobe_1_v2.index",
-)
-rvc.set_params(f0up_key=12, f0method="rmvpe")
+    rvc = RVCInference(
+        device="cuda:0",
+        model_path="mikuAI/MikuAI.pth",
+        index_path="mikuAI/added_IVF3010_Flat_nprobe_1_v2.index",
+    )
+    rvc.set_params(f0up_key=12, f0method="rmvpe")
 
-rvc.infer_file("audio.wav", "output.wav")
+    rvc.infer_file(buffer, "output.wav")
