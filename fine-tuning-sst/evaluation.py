09fff847129d9d553919d5e3adcca05448fd0d6e
41. Lattikone SITTHIPHONE 3COM2 <dragontops55@gmail.com>
2025-07-21 18:34:41 +0000

    add: denoise voice in evaluation part

--- fine-tuning-sst/evaluation.py
+++ fine-tuning-sst/evaluation.py
@@ -4,7 +4,7 @@ import csv
 import pandas as pd
 import soundfile as sf
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-from jiwer import cer
+from jiwer import cer, wer
 from tqdm import tqdm
 import argparse
 import shutil
@@ -12,6 +12,7 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str)
 parser.add_argument("--eval", type=str)
+parser.add_argument("--denoise", type=bool, default=False)
 
 model_path = ""
 
@@ -59,6 +60,49 @@ test_data = test_data[1:]
 results = []
 
 
+#################################################################################################################
+import torchaudio
+from df.enhance import enhance, init_df
+
+df_model, df_state, _ = init_df()
+
+
+def denoise_input_sound(audio_path: str) -> str:
+    waveform, sample_rate = torchaudio.load(audio_path)
+
+    resampler = torchaudio.transforms.Resample(
+        orig_freq=sample_rate, new_freq=df_state.sr()
+    )
+    waveform = resampler(waveform)
+    sample_rate = df_state.sr()
+
+    enhanced = enhance(df_model, df_state, waveform)
+
+    audio_tensor = torch.as_tensor(enhanced)
+    if audio_tensor.ndim == 1:
+        audio_tensor = audio_tensor.unsqueeze_(0)
+
+    if sample_rate != 16000:
+        resampler = torchaudio.transforms.Resample(
+            orig_freq=sample_rate, new_freq=16000
+        )
+        audio_tensor = resampler(audio_tensor)
+        sr = 16000
+    else:
+        sr = sample_rate
+
+    if audio_tensor.dtype != torch.int16:
+        audio_tensor = (audio_tensor * (1 << 15)).clamp(-32768, 32767).to(torch.int16)
+
+    des_path = "denoise_file.wav"
+    torchaudio.save(des_path, audio_tensor, sr, format="wav")
+
+    return des_path
+
+
+#################################################################################################################
+
+
 def load_audio(file):
     speech, sr = sf.read(file)
     if sr != 16000:
@@ -70,8 +114,25 @@ def load_audio(file):
 
 
 avg_error_cer = 0
+avg_error_wer = 0
 
 for audio_file, reference in tqdm(test_data):
+    if args.denoise:
+        denoise_file = denoise_input_sound(audio_file)
+
+        orig = torch.tensor(load_audio(audio_file), dtype=torch.float32)
+        denoised = torch.tensor(load_audio(denoise_file), dtype=torch.float32)
+
+        min_len = min(orig.shape[-1], denoised.shape[-1])
+        orig = orig[..., :min_len]
+        denoised = denoised[..., :min_len]
+
+        print(
+            "MSE denoise:", torch.mean((orig - denoised) ** 2)
+        )  # if high that use more denoise
+
+        audio_file = denoise_file
+
     speech = load_audio(audio_file)
     input_values = processor(
         speech, return_tensors="pt", sampling_rate=16000
@@ -86,7 +147,10 @@ for audio_file, reference in tqdm(test_data):
     ).replace("[PAD]", "")
 
     error_cer = cer(reference, transcription)
+    error_wer = wer(reference, transcription)
+
     avg_error_cer += round(error_cer, 3)
+    avg_error_wer += round(error_wer, 3)
 
     results.append(
         {
@@ -94,6 +158,7 @@ for audio_file, reference in tqdm(test_data):
             "Reference": reference,
             "Prediction": transcription,
             "CER": round(error_cer, 3),
+            "WER": round(error_wer, 3),
         }
     )
 
@@ -102,3 +167,4 @@ df.to_csv("stt_evaluation_results.csv", index=False)
 
 print(df.to_markdown())
 print("Average CER:", avg_error_cer / len(test_data))
+print("Average WER:", avg_error_wer / len(test_data))
